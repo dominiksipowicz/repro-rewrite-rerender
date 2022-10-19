a2f5e0175bf6bb30c096f41077865abd984f3aa8
Dom Sip <dominik.sipowicz@gmail.com>
2022-10-19 14:30:36 +0100

    test vercel.json

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/page-a",
-        destination: "/page-b",
-      },
-    ];
-  },
+
 };
 
 module.exports = nextConfig;
